a7a0799264d9d7d533fc93ee6ade8f5fd9e6fac8
Hassan <h23798a@gmail.com>
2023-11-15 01:51:10 +0200

    edit

--- _printf.c
+++ _printf.c
@@ -5,52 +5,47 @@
  * @format: Format string with conversion specifiers
  *
  * Return: Number of characters printed (excluding the null byte)
- * or -1 on error
  */
 int _printf(const char *format, ...)
 {
-	char c;
-	char *str;
-	int n = 0;
 	va_list args;
+	int count = 0;
+	char c;
+	const char *ptr;
+	char *str;
 
-	if (format == NULL)
-		return (-1);
 	va_start(args, format);
-	while (*format)
+	for (ptr = format; *ptr != '\0'; ptr++)
 	{
-		switch (*format)
+		if (*ptr != '%')
+			count += write(1, ptr, 1);
+		else
 		{
-		case '%':
-			switch (*++format)
+			switch (*(++ptr))
 			{
-			case 's':
-				str = va_arg(args, char *);
-				if (str != NULL)
-					n += write(1, str, strlen(str));
-				break;
 			case 'c':
 				c = (char)va_arg(args, int);
-				n += write(1, &c, 1);
+				count += write(1, &c, 1);
+				break;
+			case 's':
+
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				count += write(1, str, strlen(str));
 				break;
 			case '%':
-				c = '%';
-				n += write(1, &c, 1);
+				count += write(1, "%", 1);
 				break;
 			case 'n':
-				*va_arg(args, int *) = n;
+				*va_arg(args, int *) = count;
 				break;
 			default:
-				write(1, format - 1, 1);
-				break;
+				count += write(1, "%", 1);
+				count += write(1, ptr, 1);
 			}
-			break;
-		default:
-			n += write(1, format, 1);
-			break;
 		}
-		format++;
 	}
 	va_end(args);
-	return (n);
+	return (count);
 }
